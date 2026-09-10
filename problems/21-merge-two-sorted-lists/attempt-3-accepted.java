94ab96a32fe8ab7bf1849d2e06be25c163381951
Nitish Gupta <guptanitish2102@gmail.com>
2026-09-10 12:03:05 +0530

    Accepted: Merge Two Sorted Lists (attempt 3)

---
+++ problems/21-merge-two-sorted-lists/attempt-3-accepted.java
@@ -0,0 +1,25 @@
+class Solution {
+    public ListNode mergeTwoLists(ListNode a, ListNode b) {
+        ListNode head = new ListNode(0); 
+        ListNode temp = head;
+
+        while (a != null && b != null) {
+            if (a.val <= b.val) {
+                temp.next = a;
+                a = a.next;
+            } else {
+                temp.next = b;
+                b = b.next;
+            }
+            temp = temp.next;
+        }
+        
+        if (a != null) {
+            temp.next = a;
+        } else {
+            temp.next = b;
+        }
+
+        return head.next;
+    }
+}
