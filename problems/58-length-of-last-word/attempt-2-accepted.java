f10aa58277c57ee9646e0c3f173287d9d8b30339
Nitish Gupta <guptanitish2102@gmail.com>
2026-09-10 11:51:14 +0530

    Accepted: Length of Last Word (attempt 2)

---
+++ problems/58-length-of-last-word/attempt-2-accepted.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int length = 0;
+        int i;
+
+        
+        for (i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') {
+                break; 
+            }
+        }
+
+        
+        for (; i >= 0; i--) {
+            if (s.charAt(i) == ' ') {
+                break;
+            }
+            length++;
+        }
+
+        return length;
+    }
+}
