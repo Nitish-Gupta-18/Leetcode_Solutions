d3a13cc378979ec9d7d4a01e506c19d9db99b3a6
Nitish Gupta <guptanitish2102@gmail.com>
2026-09-10 11:50:58 +0530

    Accepted: Best Time to Buy and Sell Stock (attempt 2)

---
+++ problems/121-best-time-to-buy-and-sell-stock/attempt-2-accepted.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minPrice = prices[0];  
+
+        int maxProfit = 0;        
+
+       
+        for (int i = 1; i < prices.length; i++) {
+            
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            }
+            
+            else {
+                int todayProfit = prices[i] - minPrice;
+                if (todayProfit > maxProfit) {
+                    maxProfit = todayProfit;
+                }
+            }
+        }
+
+        return maxProfit;
+    }
+}
