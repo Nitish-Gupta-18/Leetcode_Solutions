6cfea000496c367c3c4a063eed2571acf856c3e9
Nitish Gupta <guptanitish2102@gmail.com>
2026-09-10 12:03:58 +0530

    Accepted: Find the Highest Altitude (attempt 2)

---
+++ problems/1732-find-the-highest-altitude/attempt-2-accepted.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int maxHeight = 0;
+        int currHeight = 0;
+
+        for (int i = 0; i < gain.length; i++) {
+            currHeight = currHeight + gain[i];
+
+            if (currHeight > maxHeight) {
+                maxHeight = currHeight;
+            }
+        }
+
+        return maxHeight;
+    }
+}
